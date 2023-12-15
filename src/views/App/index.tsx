8b944897d0ad241b2fda5c1c55402c5748cd9663
EkaterinaVens <83534914+EkaterinaVens@users.noreply.github.com>
2023-12-15 12:26:33 +0200

    'init'

---
+++ src/views/App/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import { useToDoStore } from '../../data/stores/useToDoStore';
+import { InputPlus } from '../components/InputPlus';
+import { InputTask } from '../components/InputTask';
+
+import styles from './index.module.scss';
+
+export const App: React.FC = () =>{
+    const [tasks, 
+        createTask,
+        updateTask,
+        removeTask
+    ] = useToDoStore(state => [
+        state.tasks,
+        state.createTask,
+        state.updateTask,
+        state.removeTask
+    ]);
+
+
+    return (
+        <article className={styles.article}>
+            <h1 className={styles.articleTitle}>План на день</h1>
+            <section className={styles.articleSection}>
+                <InputPlus 
+                onAdd={(title) => {
+                    if(title) {
+                        createTask(title)
+                    }
+                }}
+                />
+            </section>
+            <section className={styles.articleSection}>
+                {!tasks.length && (
+                    <>
+                        <p className={styles.articleText}>Давай определим дела на день?</p>
+                        <div className={styles.articleImage}></div>
+                    </>
+                )}
+                {tasks.map((task)=>(
+                    <InputTask 
+                        key={task.id}
+                        id={task.id}
+                        title={task.title}
+                        onDone={removeTask}
+                        onEdited={updateTask}
+                        onRemoved={removeTask}
+                    />))
+                }
+            </section>
+        </article>
+    );
+}
